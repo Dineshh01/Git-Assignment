79ca712de138a572651ffa9463169424d8219719
Dinesh <Dineshkuntamukkala3@gmail.com>
2023-03-02 18:04:08 +0530

    java

---
+++ ModelRakesh/src/main/java/com/example/ModelRakesh/ModelRakeshApplication.java
@@ -0,0 +1,13 @@
+package com.example.ModelRakesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelRakeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelRakeshApplication.class, args);
+	}
+
+}
---
+++ ModelRakesh/src/test/java/com/example/ModelRakesh/ModelRakeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ModelRakesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelRakeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
